b754367b4ec8e5c5bf322d6b291c13caf583734c
Christopher Wolfram <chriscwolfram@gmail.com>
2025-01-06 13:34:22 -0600

    Cleaned up scripts a bit.

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -14,11 +14,11 @@ def prepare_imdb(cache_dir, output_dir):
 if __name__ == "__main__":
      # Load command-line arguments
     cache_dir = sys.argv[1]
-    output_dir = sys.argv[2]
+    datasets_dir = sys.argv[2]
 
-    # Create output directory
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
+    # Create output directory if needed
+    if not os.path.isdir(datasets_dir):
+        os.makedirs(datasets_dir)
     
     # Authenticate for HF
     huggingface_hub.login(new_session=False)
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     # Disable caching because we don't need it
     datasets.disable_caching()
 
-    prepare_imdb(cache_dir, output_dir)
+    print("Preparing IMDB")
+    prepare_imdb(cache_dir, datasets_dir)
 
     # ds_streaming = datasets.load_dataset('stanfordnlp/imdb', split='train', streaming=True, cache_dir=cache_dir, trust_remote_code=False)
 
---
+++ scripts/prepare_datasets.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=download_models
+#SBATCH --output=logs/array_er%A_%a.out
+#SBATCH --error=logs/array_er%A_%a.err
+#SBATCH --time=240:00
+
+#SBATCH --partition=general
+#SBATCH --mem-per-cpu=8000
+#SBATCH --mail-user=chriswolfram@uchicago.edu
+#SBATCH --mail-type=END,FAIL
+
+UNIREPS_DIR="/home/chriswolfram/unireps"
+UNIREPS_HF_CACHE="/net/scratch2/chriswolfram/unireps/hf_cache"
+UNIREPS_DATASETS="/net/scratch2/chriswolfram/unireps/datasets"
+UNIREPS_OUTPUTS="/net/scratch2/chriswolfram/unireps/outputs"
+
+UNIREPS_PYTHON="$UNIREPS_DIR/.venv/bin/python"
+
+UNIREPS_PYTHON $UNIREPS_DIR/prepare_datasets.py $UNIREPS_HF_CACHE $UNIREPS_DATASETS

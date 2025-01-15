d342171c74bbbdab65cf3fc755a7bfd587ab0f5c
Christopher Wolfram <chriswolfram@uchicago.edu>
2025-01-15 16:27:29 -0600

    Updated similarity.py.

--- unireps/similarity.py
+++ unireps/similarity.py
@@ -1,31 +1,21 @@
 import torch
 
 def embs_knn(emb_mat, k):
-    """
-    Computes the k-nearest neighbors for each embedding in the given matrix using the dot product as the distance metric.
-    Args:
-        emb_mat (torch.Tensor): A 2D tensor where each row represents an embedding.
-        k (int): The number of nearest neighbors to find for each embedding.
-    Returns:
-        torch.Tensor: A 2D tensor of shape (n, k) where n is the number of embeddings. Each row contains the indices of the k-nearest neighbors for the corresponding embedding.
-    """
+
+    if len(emb_mat.shape) == 3:
+        return torch.stack([embs_knn(emb, k) for emb in emb_mat])
 
     gram = emb_mat @ emb_mat.T
     gram.fill_diagonal_(-torch.inf)
 
     return gram.topk(k, sorted=False).indices
 
-def mutual_knn(knn_mat_1, knn_mat_2):
-    """
-    Computes the mutual k-nearest neighbors between two k-nearest neighbor matrices.
-
-    Args:
-        knn_mat_1 (torch.Tensor): A 2D tensor where each row contains the indices of the k-nearest neighbors for the first set of embeddings.
-        knn_mat_2 (torch.Tensor): A 2D tensor where each row contains the indices of the k-nearest neighbors for the second set of embeddings.
-
-    Returns:
-        float: The proportion of mutual k-nearest neighbors between the two matrices.
-    """
+def _mutual_knn_tensor(knn_mat_1, knn_mat_2):
+    if len(knn_mat_1.shape) == 3:
+        return torch.stack([_mutual_knn_tensor(knn_1, knn_mat_2) for knn_1 in knn_mat_1])
+    
+    if len(knn_mat_2.shape) == 3:
+        return torch.stack([_mutual_knn_tensor(knn_mat_1, knn_2) for knn_2 in knn_mat_2])
 
     assert knn_mat_1.shape == knn_mat_2.shape
     n = knn_mat_1.shape[0]
@@ -35,7 +25,14 @@ def mutual_knn(knn_mat_1, knn_mat_2):
     for i in range(n):
         total += torch.isin(knn_mat_1[i], knn_mat_2[i], assume_unique=True).sum()
     
-    return (total / (k * n)).item()
+    return (total / (k * n))
+
+def mutual_knn(knn_mat_1, knn_mat_2):
+    out = _mutual_knn_tensor(knn_mat_1, knn_mat_2)
+    if out.dim() == 0:
+        return out.item()
+    else:
+        return out
 
 def mutual_knn_baseline(n, k):
     """

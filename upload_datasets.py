e6b08a6e7a99db5e10972b1c24f41b0cd0abc275
Christopher Wolfram <chriscwolfram@gmail.com>
2025-04-03 11:27:09 -0500

    Removed code for uploading knn for now.

--- upload_datasets.py
+++ upload_datasets.py
@@ -100,9 +100,9 @@ if __name__ == "__main__":
                 # To avoid rate limits
                 time.sleep(3)
 
-                knn_path = os.path.join(knn_dir, unireps.get_dataset_name(model_name, dataset_name, use_chat_template)) + '.parquet'
-                ds = datasets.load_dataset('knn', data_files=knn_path)
-                ds.push_to_hub('chriswolfram/knn', subset_name)
+                # knn_path = os.path.join(knn_dir, unireps.get_dataset_name(model_name, dataset_name, use_chat_template)) + '.parquet'
+                # ds = datasets.load_dataset('knn', data_files=knn_path)
+                # ds.push_to_hub('chriswolfram/knn', subset_name)
 
-                # To avoid rate limits
-                time.sleep(3)
+                # # To avoid rate limits
+                # time.sleep(3)
